ad8bcc0cc7f3e433f760e3b4233572560d45af3f
Marcos Luan <contato.mluans@gmail.com>
2024-01-11 21:50:35 -0300

    Primeiro Commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// imprime um texto na tela.
+Console.WriteLine("Hello World");
+
+Console.WriteLine("###########################################");
+Console.WriteLine("SEJA BEM VINDO AO MEU PRIMEIRO PROGRAMA");
+Console.WriteLine("###########################################");
+
+
+String nome;
+int idade;
+
+Console.WriteLine(" Qual é o seu nome?");
+nome = Console.ReadLine();
+Console.WriteLine("Prazer em conhecer " + nome + "!");
+
+Console.WriteLine(" Qual é a sua idade?");
+idade = int.Parse(Console.ReadLine());
+Console.WriteLine("Você tem " + idade + " anos de idade. ");
+
+
+
+
+
+
